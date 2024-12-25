2450d925089e40485df3638d7bf2d7718876a53b
tankorsmash <tankorsmash@gmail.com>
2024-12-25 00:13:40 -0500

    add test for making sure shifting never returns 0 for hex x or y

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -92,4 +92,30 @@ suite =
                             sourceHexAddress
                 in
                 Expect.equal res targetHexAddress
+        , Test.fuzz2
+            (Fuzz.intRange -10000 10000)
+            (Fuzz.intRange -10000 10000)
+            "shifting an address never returns a hex with 0 in either hex coordinates"
+          <|
+            \deltaX deltaY ->
+                let
+                    sourceHexAddress =
+                        HexAddress.create
+                            { sectorX = 1
+                            , sectorY = 1
+                            , x = 1
+                            , y = 1
+                            }
+
+                    res =
+                        HexAddress.shiftAddressBy
+                            { deltaX = deltaX, deltaY = deltaY }
+                            { maxX = Traveller.numHexCols, maxY = Traveller.numHexRows }
+                            sourceHexAddress
+                in
+                { x = res.x, y = res.y }
+                    |> Expect.all
+                        [ .x >> Expect.notEqual 0
+                        , .y >> Expect.notEqual 0
+                        ]
         ]
